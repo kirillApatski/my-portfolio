316e68ba6b9823fff7864f8cc5fe4383899ff958
Kirill <apatski94@gmail.com>
2023-08-20 20:26:48 +0300

    test commit -1

--- src/common/component/Buttons/Buttons.tsx
+++ src/common/component/Buttons/Buttons.tsx
@@ -1,13 +1,14 @@
-import React, {FC} from 'react';
+import React, { FC } from 'react';
 import style from './Buttons.module.css'
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 type ButtonsPropsType = {
     title: string
     path: string
 }
 
-export const Buttons: FC<ButtonsPropsType> = ({title, path}) => {
+
+export const Buttons: FC<ButtonsPropsType> = ({ title, path }) => {
     return (
         <div>
             <Link className={style.btn} to={path}>{title}</Link>
